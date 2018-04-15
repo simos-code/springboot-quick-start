e2b54fdfc4bb93ede0660d2af23bbef894ce997f
l2h <liulh@qtec.cn>
2018-04-15 14:06:58 +0800

    多环境配置

--- src/main/java/com/simos/controller/HelloController.java
+++ src/main/java/com/simos/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.simos.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    @Value("${demo.qq}")
+    String test;
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
-        return "hello world!";
+        return test;
     }
 }
